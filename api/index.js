75cfed8d8793ef8d79d4ba52a89b866b41e26232
MohitKumar <K2004mohit@gmail.com>
2026-05-02 18:58:45 +0530

    fix bug

--- api/index.js
+++ api/index.js
@@ -1,9 +1,24 @@
 const serverless = require("serverless-http");
 const { app, boot } = require("./app");
 
+let isBooted = false;
+
 const handler = serverless(app);
 
 module.exports = async (req, res) => {
-  await boot();
-  return handler(req, res);
+  try {
+    if (!isBooted) {
+      await boot();   // run only once
+      isBooted = true;
+      console.log("Boot completed");
+    }
+
+    return handler(req, res);
+  } catch (err) {
+    console.error("Server Error:", err);
+    return res.status(500).json({
+      ok: false,
+      message: "Server failed to start"
+    });
+  }
 };
